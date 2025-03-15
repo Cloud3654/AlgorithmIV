472a2c53cafa0e01b17e869c223fe61512ab3df9
Cloud3654 <163625776+Cloud3654@users.noreply.github.com>
2025-03-15 14:36:46 +0900

    Update Research.cpp

--- Research.cpp
+++ Research.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 struct Node { // Node 변수형 생성
-    vector <pair<int,int>> cld; // cld(pair-first는 자신의 번호, pair-second는 가는 데 드는 가중치) 생성
+    vector<pair<int,int>> cld; // cld(pair-first는 자신의 번호, pair-second는 가는 데 드는 가중치) 생성
     bool visit; // visit(방문 체크) 생성
 };
 
 Node nd[10]; // nd(트리) 생성
-int n, S=1, D=7, minPath = 1e9; // n(노드 수), S(시작 번호), D(목적지 번호), minPath(최소 가중치) 생성, S, D 정의
+int n, S = 1, D = 7, minPath = 1e9; // n(노드 수), S(시작 번호), D(목적지 번호), minPath(최소 가중치) 생성, S, D 정의
 
 
 void dfs (int idx, int sumWei) { // dfs(가중치 탐색 함수) 정의
@@ -19,8 +19,8 @@ void dfs (int idx, int sumWei) { // dfs(가중치 탐색 함수) 정의
         return; // 함수 종료
     }
 
-    for(auto p : nd[idx].cld) { // idx의 모든 자식 노드에 대해 반복
-        if(nd[p.first].visit == false) {  // 방문한 적이 없다면
+    for (auto p : nd[idx].cld) { // idx의 모든 자식 노드에 대해 반복
+        if (nd[p.first].visit == false) {  // 방문한 적이 없다면
             nd[p.first].visit = true; // 방문 체크
             dfs(p.first, sumWei + p.second); // 가중치에 방금 이동한 가중치를 더한 수를 2번째 인수로 dfs 호출
             nd[p.first].visit = false; // 방문 체크 해제
